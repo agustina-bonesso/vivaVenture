e660701573d38cdc44e3da8b7e56537b13f78ede
Steffen Zierold <steffen.zierold@outlook.com>
2024-06-28 08:45:14 +0200

    delete unnecessary line in styling

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -1,7 +1,6 @@
 import styled, { css } from "styled-components";
 
 export const StyledButton = styled.button`
-  /* background-color: lightsalmon; */
   padding: 0.8rem;
   border-radius: 0.6rem;
   color: black;
