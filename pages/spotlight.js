7b108e288095f97cdcff3b1ebde69a2c456410a1
Davide Esposito <davideesposito2911@gmail.com>
2024-07-08 13:06:52 +0200

    fix spotlight page

--- pages/spotlight.js
+++ pages/spotlight.js
@@ -1,33 +1,33 @@
+import { useEffect, useState } from "react";
 import ActivityCard from "@/components/ActivityCard";
-import ActivityDetails from "@/components/ActivityDetails";
-import { useRouter } from "next/router";
-
-function randomIndex(activityData) {
-  const randomIndex = Math.floor(Math.random() * activityData.length);
-  console.log(randomIndex);
-
-  return activityData[randomIndex];
-}
 
 export default function Spotlight({
   activityData,
   onToggleFavorite,
   favoriteActivitiesList,
 }) {
-  const randomActivity = randomIndex(activityData);
-  randomIndex(activityData);
+  const [randomActivity, setRandomActivity] = useState(null);
+
+  useEffect(() => {
+    const index = Math.floor(Math.random() * activityData.length);
+    setRandomActivity(activityData[index]);
+    console.log(index);
+  }, [activityData]);
+
+  if (!randomActivity) return null;
   console.log(randomActivity);
+
+  const isFavorite = favoriteActivitiesList.find(
+    (favActivity) => favActivity.id === randomActivity.id
+  );
+
   return (
     <>
       <h1>Spotlight</h1>
       <ActivityCard
         activity={randomActivity}
         onToggleFavorite={onToggleFavorite}
-        isFavorite={
-          favoriteActivitiesList.find(
-            (favActivity) => favActivity.id === randomActivity.id
-          )?.isFavorite
-        }
+        isFavorite={isFavorite}
       />
     </>
   );
