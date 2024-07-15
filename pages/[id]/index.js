3ebe795b2180fef023bf9c48cdfac9c05d8a37c6
Maria Agustina Bonesso Immerso <agustina.bonesso@gmail.com>
2024-07-15 13:28:06 +0200

    loaded image in details page error solved

---
+++ pages/[id]/index.js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import ActivityDetails from "@/components/ActivityDetails";
+
+export default function Activity({
+  activityData,
+  onDeleteActivity,
+  onToggleFavorite,
+  favoriteActivitiesList,
+}) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const activity = activityData.find((activity) => activity.id === id);
+
+  const isFavorite = favoriteActivitiesList.find(
+    (favActivity) => favActivity.id === id
+  )?.isFavorite;
+
+  return (
+    <>
+      {activity ? (
+        <ActivityDetails
+          activity={activity}
+          onDeleteActivity={onDeleteActivity}
+          isFavorite={isFavorite}
+          onToggleFavorite={onToggleFavorite}
+        />
+      ) : (
+        <p>Activity not found</p>
+      )}
+    </>
+  );
+}
