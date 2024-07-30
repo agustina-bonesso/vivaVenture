5f6790bbc6e8b73a5d980b90f086182175c04a99
Davide Esposito <davideesposito2911@gmail.com>
2024-07-30 13:06:02 +0200

    create weather component

--- components/Weather/index.js
+++ components/Weather/index.js
@@ -1,2 +1,30 @@
-import ReactWeather, { useOpenWeather } from 'react-open-weather';
+import ReactWeather, { useOpenWeather } from "react-open-weather";
+import styled from "styled-components";
 
+export default function WeatherInformation({ activity }) {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: "d96e39731c822f21ac8dc394bdfad639",
+    lat: activity.lat,
+    lon: activity.lng,
+    lang: "en",
+    unit: "metric",
+  });
+
+  return (
+    <WeatherContainer>
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        locationLabel={activity.city}
+        unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
+        showForecast
+      />
+    </WeatherContainer>
+  );
+}
+
+const WeatherContainer = styled.div`
+  margin-top: 2rem;
+`;
